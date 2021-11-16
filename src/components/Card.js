849e96c8ecb5c38ba8f2cd7c7a80dc3868cbc04e
Jing <jingwang6028@gmail.com>
2021-11-16 12:55:13 -0500

    debug url and image display issue

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,14 +3,18 @@ import React from "react";
 function Card(props) {
   return (
     <div className="card">
-      <img src="{props.image}" className="card-img-top" alt="portfolio card" />
+      <img
+        src={props.image}
+        className="card-img-top"
+        alt="portfolio card"
+      ></img>
       <div className="card-body">
         <h5 className="card-title">{props.title}</h5>
         <p className="card-text">{props.description}</p>
-        <a href="{props.github}" className="btn btn-primary">
+        <a href={props.github} className="btn btn-primary">
           GitHub
         </a>
-        <a href="{props.demo}" className="btn btn-primary">
+        <a href={props.demo} className="btn btn-primary">
           Demo
         </a>
       </div>
